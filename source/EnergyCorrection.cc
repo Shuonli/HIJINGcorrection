e591caf522560f2b71cf4f2a411d8c121a064a17
Shuonli <sli17@ole.augie.edu>
2023-11-15 20:37:29 -0500

    initial commit

---
+++ source/EnergyCorrection.cc
@@ -0,0 +1,185 @@
+//____________________________________________________________________________..
+//
+//____________________________________________________________________________..
+
+#include "EnergyCorrection.h"
+
+#include <fun4all/Fun4AllReturnCodes.h>
+
+#include <phool/PHCompositeNode.h>
+#include <phool/getClass.h>
+
+#include <phhepmc/PHHepMCGenEvent.h>
+#include <phhepmc/PHHepMCGenEventMap.h>
+
+#include <g4main/PHG4Hit.h>
+#include <g4main/PHG4HitContainer.h>
+#include <g4main/PHG4HitDefs.h>
+#include <g4main/PHG4Particle.h>
+#include <g4main/PHG4Shower.h>
+#include <g4main/PHG4TruthInfoContainer.h>
+
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
+#include <HepMC/GenEvent.h>
+#include <HepMC/GenVertex.h> // for GenVertex, GenVertex::part...
+#pragma GCC diagnostic pop
+
+#include <HepMC/GenParticle.h> // for GenParticle
+#include <HepMC/GenRanges.h>
+#include <HepMC/HeavyIon.h> // for HeavyIon
+
+#include <TFile.h>
+#include <TH1.h>
+
+//____________________________________________________________________________..
+EnergyCorrection::EnergyCorrection(const std::string &name) : SubsysReco(name) {
+  std::cout << "EnergyCorrection::EnergyCorrection(const std::string &name) "
+               "Calling ctor"
+            << std::endl;
+}
+
+//____________________________________________________________________________..
+EnergyCorrection::~EnergyCorrection() {
+  std::cout << "EnergyCorrection::~EnergyCorrection() Calling dtor"
+            << std::endl;
+}
+
+//____________________________________________________________________________..
+int EnergyCorrection::Init(PHCompositeNode *topNode) {
+  std::cout << "EnergyCorrection::Init(PHCompositeNode *topNode) Initializing"
+            << std::endl;
+  // read correction histogram from file
+  TFile *f_upweight =
+      new TFile("/sphenix/user/shuhangli/dETdeta/macro/fitratio.root");
+  std::string postfix[5] = {"0010_ratio", "1020_ratio", "2040_ratio",
+                            "4060_ratio", "6092_ratio"};
+  for (int i = 0; i < 5; i++) {
+    h_pimi[i] = (TH1F *)f_upweight->Get(Form("h_pimi%s", postfix[i].c_str()));
+    h_pip[i] = (TH1F *)f_upweight->Get(Form("h_pip%s", postfix[i].c_str()));
+    h_p[i] = (TH1F *)f_upweight->Get(Form("h_p%s", postfix[i].c_str()));
+    h_pbar[i] = (TH1F *)f_upweight->Get(Form("h_pbar%s", postfix[i].c_str()));
+    h_kp[i] = (TH1F *)f_upweight->Get(Form("h_kp%s", postfix[i].c_str()));
+    h_kmi[i] = (TH1F *)f_upweight->Get(Form("h_kmi%s", postfix[i].c_str()));
+  }
+
+  return Fun4AllReturnCodes::EVENT_OK;
+}
+
+//____________________________________________________________________________..
+int EnergyCorrection::process_event(PHCompositeNode *topNode) {
+  if (Verbosity() > 0)
+    std::cout << "EnergyCorrection::process_event(PHCompositeNode *topNode) "
+                 "Processing Event"
+              << std::endl;
+
+  PHHepMCGenEventMap *genevtmap =
+      findNode::getClass<PHHepMCGenEventMap>(topNode, "PHHepMCGenEventMap");
+  if (!genevtmap) {
+    std::cout << "no genevtmap" << std::endl;
+    return Fun4AllReturnCodes::ABORTEVENT;
+  }
+  for (PHHepMCGenEventMap::Iter iter = genevtmap->begin();
+       iter != genevtmap->end(); ++iter) {
+    PHHepMCGenEvent *genevt = iter->second;
+    // check if embedded
+    if (genevt->get_embedding_id() != 0)
+      continue;
+    HepMC::GenEvent *event = genevt->getEvent();
+    if (!event) {
+      std::cout << PHWHERE << " no evt pointer under HEPMC Node found"
+                << std::endl;
+    } else {
+      HepMC::HeavyIon *hi = event->heavy_ion();
+      if (!hi) {
+        std::cout << PHWHERE
+                  << ": Fermi Motion Afterburner needs the Heavy Ion Event "
+                     "Info, GenEvent::heavy_ion() returns NULL"
+                  << std::endl;
+        exit(1);
+      }
+      m_npart = hi->Npart_proj() + hi->Npart_targ();
+    }
+  }
+  if (m_npart < 0) {
+    std::cout << "cant find npart" << std::endl;
+    return Fun4AllReturnCodes::ABORTEVENT;
+  }
+
+  // get truthinfo
+  PHG4TruthInfoContainer *truthinfo =
+      findNode::getClass<PHG4TruthInfoContainer>(topNode, "G4TruthInfo");
+  // get primary particles
+  if (!truthinfo) {
+    std::cout << "EnergyCorrection::process_event(PHCompositeNode *topNode) "
+                 "Could not locate G4TruthInfo node"
+              << std::endl;
+    return Fun4AllReturnCodes::ABORTEVENT;
+  }
+
+  PHG4TruthInfoContainer::Range range = truthinfo->GetPrimaryParticleRange();
+
+  for (PHG4TruthInfoContainer::Iterator iter = range.first;
+       iter != range.second; ++iter) {
+    PHG4Particle *particle = iter->second;
+    int pid = particle->get_pid();
+    float pt = sqrt(particle->get_px() * particle->get_px() +
+                    particle->get_py() * particle->get_py());
+    float scale = findcorrection(m_npart, pid, pt);
+    particle->set_e(particle->get_e() * scale);
+    particle->set_px(particle->get_px() * scale);
+    particle->set_py(particle->get_py() * scale);
+    particle->set_pz(particle->get_pz() * scale);
+  }
+
+  // get hits
+  PHG4HitContainer *hits =
+      findNode::getClass<PHG4HitContainer>(topNode, m_HitNodeName);
+  if (!hits) {
+    std::cout << "EnergyCorrection::process_event(PHCompositeNode *topNode) "
+                 "Could not locate g4 hit node "
+              << m_HitNodeName << std::endl;
+    return Fun4AllReturnCodes::ABORTEVENT;
+  }
+  PHG4HitContainer::ConstRange hit_range = hits->getHits();
+  for (PHG4HitContainer::ConstIterator hit_iter = hit_range.first;
+       hit_iter != hit_range.second; hit_iter++) {
+    PHG4Hit *hit = hit_iter->second;
+    int showerid = hit_iter->second->get_shower_id();
+    PHG4Shower *shower = truthinfo->GetPrimaryShower(showerid);
+    if (!shower) {
+      if (Verbosity() > 0)
+        std::cout << "EnergyCorrection::process_event(PHCompositeNode "
+                     "*topNode) No shower found showerid: "
+                  << showerid << std::endl;
+      continue;
+    }
+    int trkid = shower->get_parent_particle_id();
+    PHG4Particle *part = truthinfo->GetParticle(trkid);
+    if (!part) {
+      std::cout << "EnergyCorrection::process_event(PHCompositeNode *topNode) "
+                   "No parent particle found, track id: "
+                << trkid << std::endl;
+    }
+    // get particle pid and pt
+    int pid = part->get_pid();
+    float pt =
+        sqrt(part->get_px() * part->get_px() + part->get_py() * part->get_py());
+    // find correction factor for G4Hits
+    float scale = findcorrection(m_npart, pid, pt);
+
+    // apply correction
+    hit->set_edep(hit->get_edep() * scale);
+    hit->set_light_yield(hit->get_light_yield() * scale);
+  }
+
+  return Fun4AllReturnCodes::EVENT_OK;
+}
+
+//____________________________________________________________________________..
+int EnergyCorrection::End(PHCompositeNode *topNode) {
+  std::cout
+      << "EnergyCorrection::End(PHCompositeNode *topNode) This is the End..."
+      << std::endl;
+  return Fun4AllReturnCodes::EVENT_OK;
+}
---
+++ source/EnergyCorrection.h
@@ -0,0 +1,169 @@
+// Tell emacs that this is a C++ source
+//  -*- C++ -*-.
+#ifndef ENERGYCORRECTION_H
+#define ENERGYCORRECTION_H
+
+#include <fun4all/SubsysReco.h>
+#include <TH1.h>
+
+#include <string>
+
+class PHCompositeNode;
+
+class EnergyCorrection : public SubsysReco
+{
+public:
+    EnergyCorrection(const std::string &name = "EnergyCorrection");
+
+    ~EnergyCorrection() override;
+
+    int Init(PHCompositeNode *topNode) override;
+
+    /** Called for first event when run number is known.
+        Typically this is where you may want to fetch data from
+        database, because you know the run number. A place
+        to book histograms which have to know the run number.
+     */
+
+    /** Called for each event.
+        This is where you do the real work.
+     */
+    int process_event(PHCompositeNode *topNode) override;
+
+    /// Called at the end of all processing.
+    int End(PHCompositeNode *topNode) override;
+
+    void SetHitNodeName(const std::string &name) { m_HitNodeName = name; }
+
+private:
+    std::string m_HitNodeName = "G4HIT_CEMC";
+
+    int m_npart =  -1;
+
+    static const int ncentbins = 5;
+    TH1F *h_pimi[5] = {nullptr};
+    TH1F *h_pip[5] = {nullptr};
+    TH1F *h_p[5] = {nullptr};
+    TH1F *h_pbar[5] = {nullptr};
+    TH1F *h_kp[5] = {nullptr};
+    TH1F *h_kmi[5] = {nullptr};
+
+    
+    const float avgcent[ncentbins] = {325.8, 236.1, 141.5, 61.6, 14.7};
+    
+
+    float findcorrection(int npart, int pid, float pt)
+    {
+        float weight[ncentbins] = {0};
+        float scale = 0;
+
+        if (npart > avgcent[0] || npart < avgcent[ncentbins - 1])
+        {
+            if (npart > avgcent[0])
+                weight[0] = 1;
+            if (npart < avgcent[ncentbins - 1])
+                weight[ncentbins - 1] = 1;
+        }
+        else
+        {
+            // use interpolation here
+            // first find which two bins the npart falls in between
+            int lowerBin = -1;
+            int upperBin = -1;
+
+            for (int i = 0; i < ncentbins - 1; i++)
+            {
+                if (npart <= avgcent[i] && npart >= avgcent[i + 1])
+                {
+                    lowerBin = i;
+                    upperBin = i + 1;
+                    break;
+                }
+            }
+            // interpolate
+            weight[upperBin] = (avgcent[lowerBin] - npart) / (avgcent[lowerBin] - avgcent[upperBin]);
+            weight[lowerBin] = (npart - avgcent[upperBin]) / (avgcent[lowerBin] - avgcent[upperBin]);
+            // print all weights and the sum
+        }
+        // if pi0 then 50% chance use the correction for pi+ and 50% chance use the correction for pi-
+        if (pid == 111)
+        {
+            pid = 211;
+            if (rand() % 2 == 0)
+            {
+                pid = -211;
+            }
+        }
+        if (pid == 130 || pid == 310 || pid == 311)
+        {
+            pid = 211;
+            if (rand() % 2 == 0)
+            {
+                pid = -211;
+            }
+        }
+        if (pid == 211)
+        {
+            // loop over cent bins
+            for (int i = 0; i < ncentbins; i++)
+            {
+
+                scale += weight[i] * h_pip[i]->Interpolate(pt);
+            }
+        }
+        else if (pid == -211)
+        {
+            // loop over cent bins
+            for (int i = 0; i < ncentbins; i++)
+            {
+                scale += weight[i] * h_pimi[i]->Interpolate(pt);
+            }
+        }
+        else if (pid > 2000 && pid < 4000)
+        {
+
+            // loop over cent bins
+            for (int i = 0; i < ncentbins; i++)
+            {
+                scale += weight[i] * h_p[i]->Interpolate(pt);
+            }
+        }
+        else if (pid < -2000 && pid > -4000)
+        {
+
+            // loop over cent bins
+            for (int i = 0; i < ncentbins; i++)
+            {
+                scale += weight[i] * h_pbar[i]->Interpolate(pt);
+            }
+        }
+        // k+
+
+        else if (pid == 321)
+        {
+            // loop over cent bins
+            for (int i = 0; i < ncentbins; i++)
+            {
+                scale += weight[i] * h_kp[i]->Interpolate(pt);
+            }
+        }
+        // k-
+        else if (pid == -321)
+        {
+            // loop over cent bins
+            for (int i = 0; i < ncentbins; i++)
+            {
+
+                scale += weight[i] * h_kmi[i] -> Interpolate(pt);
+            }
+        }
+        else
+        {
+            scale = 1;
+        }
+       
+        return scale;
+    }
+};
+
+#endif // ENERGYCORRECTION_H
